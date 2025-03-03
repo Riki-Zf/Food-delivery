41dc84b08107128e8418681ce624d3b838791c44
Riki-Zf <rikipatande81979@gmail.com>
2025-03-03 22:25:35 +0800

    add explore menu section (fe)

---
+++ frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Home.css";
+import Header from "../../components/header/Header";
+import Exploremenu from "../../components/ExploreMenu/Exploremenu";
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <Exploremenu />
+    </div>
+  );
+};
+
+export default Home;
