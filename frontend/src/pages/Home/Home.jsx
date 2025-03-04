39c43659aaf0f348c1e45da2ec5005167bd018f8
Riki-Zf <rikipatande81979@gmail.com>
2025-03-04 22:05:24 +0800

    add list menu

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Home.css";
 import Header from "../../components/header/Header";
 import Exploremenu from "../../components/ExploreMenu/Exploremenu";
+import FoodDisplay from "../../components/Food Display/FoodDisplay";
 const Home = () => {
+  const [category, setCategory] = useState("All");
   return (
     <div>
       <Header />
-      <Exploremenu />
+      <Exploremenu category={category} setCategory={setCategory} />
+      <FoodDisplay category={category} />
     </div>
   );
 };
