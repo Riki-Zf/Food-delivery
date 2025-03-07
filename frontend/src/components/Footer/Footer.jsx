a00657cbd916b6e5bfa2c75ad485358b5502c4bf
Riki-Zf <rikipatande81979@gmail.com>
2025-03-07 22:10:30 +0800

    add footers

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import "./Footer.css";
+import { assets } from "../../assets/assets";
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="footer-content">
+        <div className="footer-content-left">
+          <img src={assets.logo} alt="" />
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero excepturi officia enim quam atque error hic culpa minima, alias, assumenda, fugiat tempore facere a ipsa.</p>
+          <div className="footer-social-icons">
+            <img src={assets.facebook_icon} alt="" />
+            <img src={assets.twitter_icon} alt="" />
+            <img src={assets.linkedin_icon} alt="" />
+          </div>
+        </div>
+        <div className="footer-content-center">
+          <h2>COMPANY</h2>
+          <ul>
+            <li>Home</li>
+            <li>About us</li>
+            <li>Delivery</li>
+            <li>Privacy Policy</li>
+          </ul>
+        </div>
+        <div className="footer-content-right">
+          <h2>Get in Touch</h2>
+          <ul>
+            <li>0854-4444-2222</li>
+            <li>tomato@gmail.com</li>
+          </ul>
+        </div>
+      </div>
+      <hr />
+      <p className="footer-copyright">Copyright 2025 &copy; jagoanmama. All Rights Reserved</p>
+    </div>
+  );
+};
+
+export default Footer;
