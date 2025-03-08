85062477273d90a32a92fef6c401dc253255c670
Riki-Zf <rikipatande81979@gmail.com>
2025-03-08 22:15:05 +0800

    homepage completed

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import "./Home.css";
 import Header from "../../components/header/Header";
 import Exploremenu from "../../components/ExploreMenu/Exploremenu";
 import FoodDisplay from "../../components/Food Display/FoodDisplay";
+import AppDownload from "../../components/AppDownload/AppDownload";
 const Home = () => {
   const [category, setCategory] = useState("All");
   return (
@@ -10,6 +11,7 @@ const Home = () => {
       <Header />
       <Exploremenu category={category} setCategory={setCategory} />
       <FoodDisplay category={category} />
+      <AppDownload />
     </div>
   );
 };
