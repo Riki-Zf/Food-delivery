bee2273313d2e6957851b7cbc10037c31eb25f0c
Riki-Zf <rikipatande81979@gmail.com>
2025-03-09 22:15:20 +0800

    make web responsive

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import "./Navbar.css";
 import { assets } from "../../assets/assets";
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
   //menu underline
   const [menu, setMenu] = useState("home");
@@ -8,18 +10,18 @@ const Navbar = () => {
     <div className="navbar">
       <img src={assets.logo} alt="" className="logo" />
       <ul className="navbar-menu">
-        <li onClick={() => setMenu("home")} className={menu === "home" ? "active" : ""}>
+        <Link to="/" onClick={() => setMenu("home")} className={menu === "home" ? "active" : ""}>
           home
-        </li>
-        <li onClick={() => setMenu("menu")} className={menu === "menu" ? "active" : ""}>
+        </Link>
+        <a href="#explore-menu" onClick={() => setMenu("menu")} className={menu === "menu" ? "active" : ""}>
           menu
-        </li>
-        <li onClick={() => setMenu("mobile-app")} className={menu === "mobile-app" ? "active" : ""}>
+        </a>
+        <a href="#app-download" onClick={() => setMenu("mobile-app")} className={menu === "mobile-app" ? "active" : ""}>
           mobile app
-        </li>
-        <li onClick={() => setMenu("contact-us")} className={menu === "contact-us" ? "active" : ""}>
+        </a>
+        <a href="#footer" onClick={() => setMenu("contact-us")} className={menu === "contact-us" ? "active" : ""}>
           contact us
-        </li>
+        </a>
       </ul>
       <div className="navbar-right">
         <img src={assets.search_icon} alt="" />
