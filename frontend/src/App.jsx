be1d1e242e2409ff51d8b9941fb340ba52b4677e
Riki-Zf <rikipatande81979@gmail.com>
2025-04-08 21:00:43 +0800

    starting coding again, login popup update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
   const [showLogin, setShowLogin] = useState(false);
   return (
     <>
-      {showLogin ? <LoginPopUp /> : <></>}
+      {showLogin ? <LoginPopUp setShowLogin={setShowLogin} /> : <></>}
       <div className="app">
         <Navbar setShowLogin={setShowLogin} />
         <Routes>
--- frontend/src/components/LoginPopUp/LoginPopUp.jsx
+++ frontend/src/components/LoginPopUp/LoginPopUp.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import "./LoginPopUp.css";
-const LoginPopUp = () => {
-  return <div>tes</div>;
+import { assets } from "../../assets/assets";
+const LoginPopUp = ({ setShowLogin }) => {
+  const [currentState, setCurrentState] = useState("Login");
+  return (
+    <div className="login-popup">
+      <form className="login-popup-container">
+        <div className="login-popup-title">
+          <h2>{currentState}</h2>
+          <img onClick={() => setShowLogin(false)} src={assets.cross_icon} alt="" />
+        </div>
+        <div className="login-popup-inputs">
+          {currentState === "Login" ? <></> : <input type="text" placeholder="your name" required />}
+
+          <input type="email" placeholder="your email" required />
+          <input type="password" placeholder="your password" required />
+        </div>
+        <button>{currentState === "Sign Up" ? "Create account" : "Login"}</button>
+        <div className="login-popup-condition">
+          <input type="checkbox" required />
+          <p>Lorem ipsum dolor sit amet.</p>
+        </div>
+        {currentState === "Login" ? (
+          <p>
+            Create a new account <span onClick={() => setCurrentState("Sign Up")}>Click here</span>
+          </p>
+        ) : (
+          <p>
+            Have an account? <span onClick={() => setCurrentState("Login")}>Sign here</span>
+          </p>
+        )}
+      </form>
+    </div>
+  );
 };
 
 export default LoginPopUp;
