4e5bfd2510a0160da660ec2b75595e737456d9e0
Riki-Zf <rikipatande81979@gmail.com>
2025-05-01 07:28:54 +0800

    completed cart page

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -15,16 +15,24 @@ const StoreContextProvider = (props) => {
   const removeFromCart = (itemId) => {
     setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
   };
+  const getTotalCartAmount = () => {
+    let totalAmount = 0;
+    for (const item in cartItems) {
+      if (cartItems[item] > 0) {
+        let itemInfo = food_list.find((product) => product._id === item);
+        totalAmount += itemInfo.price * cartItems[item];
+      }
+    }
+    return totalAmount;
+  };
 
-  useEffect(() => {
-    console.log(cartItems);
-  }, [cartItems]);
   const contextValue = {
     food_list,
     cartItems,
     setCartItems,
     addCart,
     removeFromCart,
+    getTotalCartAmount,
   };
   return <StoreContext.Provider value={contextValue}>{props.children}</StoreContext.Provider>;
 };
