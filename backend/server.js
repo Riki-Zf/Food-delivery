466fef24db4b6fa3705d8fadd89a4fb2154170d1
Riki-Zf <rikipatande81979@gmail.com>
2025-05-08 06:28:36 +0800

    add backend setup

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,17 @@
-console.log("hello");
+import express from "express";
+import cors from "cors";
+
+// app config
+const app = express();
+const port = 4000;
+//middleware
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello from backend");
+});
+
+app.listen(port, () => {
+  console.log(`server running on http://localhost:${port}`);
+});
